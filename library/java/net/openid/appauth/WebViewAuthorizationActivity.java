9b375bdb9f4871ac4457a5eae49ba7be2a56405b
Frederik Eichner <frederik.eichner@bayoo.net>
2025-03-20 21:00:41 +0100

    AppAuth in WebView test

--- library/java/net/openid/appauth/WebViewAuthorizationActivity.java
+++ library/java/net/openid/appauth/WebViewAuthorizationActivity.java
@@ -1,6 +1,7 @@
 package net.openid.appauth;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
@@ -9,47 +10,77 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class WebViewAuthorizationActivity extends AppCompatActivity {
 
-    private WebView webView;
-    public static final String AUTH_URL_KEY = "AUTH_URL";
+    private static final String EXTRA_AUTH_REQUEST = "authRequest";
+    public static final String ACTION_AUTHORIZATION = "net.openid.appauth.HANDLE_AUTHORIZATION_RESPONSE";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        webView = new WebView(this);
+
+        WebView webView = new WebView(this);
         setContentView(webView);
 
-        String authUrl = getIntent().getStringExtra(AUTH_URL_KEY);
-        if (authUrl != null) {
-            webView.getSettings().setJavaScriptEnabled(true);
-            webView.setWebViewClient(new WebViewClient() {
-                @Override
-                public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                    String url = null;
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                        url = request.getUrl().toString();
-                    }
-                    if (url.startsWith("<YOUR_REDIRECT_URI>")) {
-                        handleRedirect(url);
-                        return true;
-                    }
-                    return false;
-                }
-            });
-            webView.loadUrl(authUrl);
-        } else {
+        AuthorizationRequest authRequest = getIntent().getParcelableExtra(EXTRA_AUTH_REQUEST);
+        if (authRequest == null) {
             finish();
+            return;
         }
+
+        // Configure WebView settings
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+
+        // Set up WebViewClient to intercept redirect URL
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = null;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    uri = request.getUrl();
+                }
+
+                // Check if the URL matches the redirect URI
+                if (uri.toString().startsWith(authRequest.redirectUri.toString())) {
+                    // Extract response parameters
+                    Intent responseIntent = new Intent();
+                    responseIntent.setData(uri);
+
+                    // Create response from redirect URI
+                    AuthorizationResponse response = new AuthorizationResponse.Builder(authRequest)
+                        .fromUri(uri)
+                        .build();
+                    //AuthorizationResponse response = AuthorizationResponse.fromUri(uri);
+                    AuthorizationException ex = AuthorizationException.fromOAuthRedirect(uri);
+
+                    if (response != null) {
+                        responseIntent.putExtra(AuthorizationResponse.EXTRA_RESPONSE, response.toIntent());
+                    }
+
+                    if (ex != null) {
+                        responseIntent.putExtra(AuthorizationException.EXTRA_EXCEPTION, ex);
+                    }
+
+                    setResult(RESULT_OK, responseIntent);
+                    finish();
+                    return true;
+                }
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+
+        // Load authorization URL
+        webView.loadUrl(authRequest.toUri().toString());
     }
 
-    private void handleRedirect(String url) {
-        Intent resultIntent = new Intent();
-        resultIntent.setData(Uri.parse(url));
-        setResult(RESULT_OK, resultIntent);
-        finish();
+    public static Intent createStartIntent(Activity activity, AuthorizationRequest request) {
+        Intent intent = new Intent(activity, WebViewAuthorizationActivity.class);
+        intent.putExtra(EXTRA_AUTH_REQUEST, request.jsonSerializeString());
+        return intent;
     }
 }
