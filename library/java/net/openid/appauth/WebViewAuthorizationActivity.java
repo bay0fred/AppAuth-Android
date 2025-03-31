1d152069ee7fb9c3b161320bb1850746d29ab818
BAYOO\frederik.eichner <frederik.eichner@bayoo.net>
2025-03-31 15:44:51 +0200

    Clean up of WebViewAuthorizationActivity

--- library/java/net/openid/appauth/WebViewAuthorizationActivity.java
+++ library/java/net/openid/appauth/WebViewAuthorizationActivity.java
@@ -1,10 +1,10 @@
 package net.openid.appauth;
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -58,14 +58,14 @@ public class WebViewAuthorizationActivity extends AppCompatActivity {
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
 
-        CredentialManagerHandler credentialManagerHandler = new CredentialManagerHandler(this);
-        Boolean listenerSupported = WebViewFeature.isFeatureSupported(
+        boolean listenerSupported = WebViewFeature.isFeatureSupported(
             WebViewFeature.WEB_MESSAGE_LISTENER
         );
         if (listenerSupported) {
+
             // Inject local JavaScript that calls Credential Manager.
-            //hookWebAuthnWithListener(webView, this, coroutineScope, credentialManagerHandler);
             CoroutineScope coroutineScope = LifecycleOwnerKt.getLifecycleScope(this);
+            CredentialManagerHandler credentialManagerHandler = new CredentialManagerHandler(this);
             PasskeyWebListener passkeyWebListener = new PasskeyWebListener(
                 this,
                 coroutineScope,
@@ -107,7 +107,6 @@ public class WebViewAuthorizationActivity extends AppCompatActivity {
             });
 
             Set<String> rules = new HashSet<>(Arrays.asList("*"));
-
             if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_MESSAGE_LISTENER)) {
                 WebViewCompat.addWebMessageListener(
                     webView, PasskeyWebListener.INTERFACE_NAME, rules, passkeyWebListener
@@ -116,6 +115,7 @@ public class WebViewAuthorizationActivity extends AppCompatActivity {
 
         } else {
 
+            // Setup web view without WebAuthn hook
             webView.setWebViewClient(new WebViewClient() {
                 @Override
                 public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
@@ -139,45 +139,6 @@ public class WebViewAuthorizationActivity extends AppCompatActivity {
             });
         }
 
-        // Set up WebViewClient to intercept redirect URL
-//        webView.setWebViewClient(new WebViewClient() {
-//            @Override
-//            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-//                Uri uri = null;
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//                    uri = request.getUrl();
-//                }
-//
-//                // Check if the URL matches the redirect URI
-//                if (uri.toString().startsWith(finalAuthRequest.redirectUri.toString())) {
-//
-//                    // Create response from redirect URI
-//                    AuthorizationResponse response = new AuthorizationResponse.Builder(finalAuthRequest)
-//                        .fromUri(uri)
-//                        .build();
-//
-//                    setResult(RESULT_OK, response.toIntent());
-//                    finish();
-//                    return true;
-//                }
-//                return super.shouldOverrideUrlLoading(view, request);
-//            }
-//
-//            @Override
-//            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-//                super.onPageStarted(view, url, favicon);
-//                passkeyWebListener.onPageStarted();
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//                    webView.evaluateJavascript(PasskeyWebListener.INJECTED_VAL, null);
-//                }
-//            }
-//
-//            @Override
-//            public void onPageFinished(WebView view, String url) {
-//                super.onPageFinished(view, url);
-//            }
-//        });
-
         // Load authorization URL
         webView.loadUrl(authRequest.toUri().toString());
     }
@@ -187,40 +148,4 @@ public class WebViewAuthorizationActivity extends AppCompatActivity {
         intent.putExtra(EXTRA_AUTH_REQUEST, request.jsonSerializeString());
         return intent;
     }
-
-//    public static void hookWebAuthnWithListener(
-//        WebView webView,
-//        Activity activity,
-//        CoroutineScope coroutineScope,
-//        CredentialManagerHandler credentialManagerHandler
-//    ) {
-//        PasskeyWebListener passkeyWebListener = new PasskeyWebListener(activity, coroutineScope, credentialManagerHandler);
-//
-//        WebViewClient webViewClient = new WebViewClient() {
-//            @Override
-//            public void onPageStarted(WebView view, String url, Bitmap favicon) {
-//                super.onPageStarted(view, url, favicon);
-//                passkeyWebListener.onPageStarted();
-//                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//                    webView.evaluateJavascript(PasskeyWebListener.INJECTED_VAL, null);
-//                }
-//            }
-//
-//            @Override
-//            public void onPageFinished(WebView view, String url) {
-//                super.onPageFinished(view, url);
-//            }
-//
-//        };
-//
-//        webView.setWebViewClient(webViewClient);
-//
-//        Set<String> rules = new HashSet<>(Arrays.asList("*"));
-//
-//        if (WebViewFeature.isFeatureSupported(WebViewFeature.WEB_MESSAGE_LISTENER)) {
-//            WebViewCompat.addWebMessageListener(
-//                webView, PasskeyWebListener.INTERFACE_NAME, rules, passkeyWebListener
-//            );
-//        }
-//    }
 }
